a75116d4be46c68f1ff010fd60476550094bd82e
FatimaOrozco02 <forozco@difusion.com.mx>
2026-06-17 10:36:52 -0600

    se agrega el favicon y se cambia la imagen del footer

--- resources/layouts/footer.php
+++ resources/layouts/footer.php
@@ -34,27 +34,23 @@
 
 
 <footer class="text-white p-0 position-relative w-100 overflow-hidden" 
-        style="min-height: 260px; height: auto; background-color: #000;">
+        style="min-height: 300px; height: auto; background-color: #000;">
    
-    
+    <!-- FONDO AJUSTADO: Cambiado 'contain' por 'cover' para centrar y expandir la imagen correctamente -->
     <div class="position-absolute top-0 start-0 w-100 h-100 d-none d-lg-block" 
-         style="background: url('<?php echo $baseUrl ?>img/banner diestro footer copia.jpg') center center / contain no-repeat;">
+         style="background: url('<?php echo $baseUrl ?>img/banner diestro footer copia 2.jpg') center center / cover no-repeat;">
     </div>
    
-    
     <div class="container-fluid px-3 px-md-5 position-relative z-1" style="max-width: 1440px;">
         
-       
         <div class="row align-items-center justify-content-center justify-content-lg-start" 
              style="min-height: 140px; @media (min-width: 992px) { min-height: 260px; }">
             
-           
             <div class="col-12 col-lg-4 text-center text-lg-start py-4 py-lg-0">
                 <div class="mx-auto mx-lg-0" style="max-width: 160px;">
                    <img src="<?php echo $baseUrl ?>img/logo diestro_1200px_blanco.png" alt="Diestro Fortalecemos Negocios" class="img-fluid">
                 </div>
             </div>
-            
             
             <div class="col-lg-8 d-none d-lg-block"></div>
             
